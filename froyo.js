0a128a4ef18e7670540ca00698509d93dc39ff59
Raven Hendricks <ravenkbeats@gmail.com>
2024-01-16 11:48:31 -0500

    simplified user input retrieval

--- froyo.js
+++ froyo.js
@@ -1,16 +1,25 @@
-const froyoInputMessage = alert("What flavor(s) of frozen yogurt would you like? We offer vanilla, strawberry and coffee. Please list all choices separated by a comma.");
+const froyoInput = prompt('What flavors of frozen yogurt would you like?');
 
-document.getElementById('order').addEventListener('submit', retrieveOrder);
-// when the submit button is pressed by the user, it will run the retrieveOrder function
+const customerArray = froyoInput.split(","); // separates user input by commas soon to be placed in array
 
-function retrieveOrder(){
-    const inputOrder = []; //empty array to store user input
-    const userFlavor = document.getElementById('flavorInput').value; //retrieves user input
-    inputOrder.push(userFlavor); //adds all user input
-    console.log(inputOrder);
+const orderArray = [];
+
+for (i = 0; i < customerArray.length; i++){
+    //organize customerArray into an actual array
+    const flavor = customerArray[i];
+    orderArray.push(flavor);
+    console.log(orderArray);
 }
 
-/*function displayOrder(){
-    const customerOrder = ["vanilla","vanilla","vanilla","strawberry","coffee","coffee"];
+function displayOrder(orderArray){
+    //build object to be updated based on current userinput
+    const froyoOrderTable = {}
+    for (i = 0; i < orderArray.length; i++){
+        let currentOrder = orderArray[i];
+        if (froyoOrderTable[currentOrder]) {
+            froyoOrderTable.push(currentOrder);
+        } else {
+
+        }
+    }
 }
-*/
