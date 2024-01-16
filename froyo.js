cb5933ebefb3b92de98ff64e2767e733d46f2533
Raven Hendricks <ravenkbeats@gmail.com>
2024-01-16 12:11:26 -0500

    fixed typo that was preventing table from being visible)

--- froyo.js
+++ froyo.js
@@ -1,25 +1,29 @@
 const froyoInput = prompt('What flavors of frozen yogurt would you like?');
 
-const customerArray = froyoInput.split(","); // separates user input by commas soon to be placed in array
+const customerArray = froyoInput.split(", "); // separates user input by commas soon to be placed in array
 
 const orderArray = [];
-
 for (i = 0; i < customerArray.length; i++){
     //organize customerArray into an actual array
     const flavor = customerArray[i];
     orderArray.push(flavor);
-    console.log(orderArray);
 }
+
+console.log(orderArray);
 
 function displayOrder(orderArray){
     //build object to be updated based on current userinput
-    const froyoOrderTable = {}
+    let froyoOrderTable = {};
     for (i = 0; i < orderArray.length; i++){
         let currentOrder = orderArray[i];
-        if (froyoOrderTable[currentOrder]) {
-            froyoOrderTable.push(currentOrder);
+        if (froyoOrderTable[currentOrder]){
+            froyoOrderTable[currentOrder] ++;
         } else {
-
+            froyoOrderTable[currentOrder] = 1;
         }
     }
+    return froyoOrderTable;
 }
+
+console.table(displayOrder(customerArray
+    ));
