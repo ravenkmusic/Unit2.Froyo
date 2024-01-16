093e1398af3e4cd6c4e49a7f4eff37824ff13929
Raven Hendricks <ravenkbeats@gmail.com>
2024-01-16 09:29:44 -0500

    created function to retrieve user input

--- froyo.js
+++ froyo.js
@@ -1,1 +1,16 @@
-const froyoInput = alert("What flavor(s) of frozen yogurt would you like and how many would you like of each? We offer vanilla, strawberry and coffee.");
+const froyoInputMessage = alert("What flavor(s) of frozen yogurt would you like? We offer vanilla, strawberry and coffee. Please list all choices separated by a comma.");
+
+document.getElementById('order').addEventListener('submit', retrieveOrder);
+// when the submit button is pressed by the user, it will run the retrieveOrder function
+
+function retrieveOrder(){
+    const inputOrder = []; //empty array to store user input
+    const userFlavor = document.getElementById('flavorInput').value; //retrieves user input
+    inputOrder.push(userFlavor); //adds all user input
+    console.log(inputOrder);
+}
+
+/*function displayOrder(){
+    const customerOrder = ["vanilla","vanilla","vanilla","strawberry","coffee","coffee"];
+}
+*/
